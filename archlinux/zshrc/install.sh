e813bf7edc4d037dc155497d3187c551d3de4800
Martin Musale <martinmshale@gmail.com>
2018-04-05 15:31:20 +0300

    Download zsh and set it up

--- archlinux/zshrc/install.sh
+++ archlinux/zshrc/install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 echo "Installing zsh..."
-sudo pacman -S --noconfirm zsh curl
+sudo pacman -S --noconfirm zsh
 echo "Done installing zsh. Installing oh-my-zsh..."
 curl -L http://install.ohmyz.sh | sh
 echo "Done installing oh-my-zsh. Changing shell to zsh..."
+cp .zshrc $HOME/.zshrc
 chsh -s /bin/zsh
 echo "Done changing shell to zsh."
