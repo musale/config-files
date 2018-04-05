e770106f568fda9c050e8dcb5b7449a1635a8622
Martin Musale <martinmshale@gmail.com>
2018-04-05 16:14:32 +0300

    Refactor git installation script

--- archlinux/git/install.sh
+++ archlinux/git/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 BLUE='\033[1;34m'
 GITCONFIG=$HOME/.gitconfig
+CONFIG=config
 echo "${BLUE}Creating the git config..."
-cp config GITCONFIG
+cp $CONFIG $GITCONFIG
 echo "${BLUE}Done creating the git config."
