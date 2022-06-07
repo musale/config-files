3bacea9c1f7aa1c061a1557976a486c6e8a3cac1
Musale Martin <martinmusale@microsoft.com>
2022-06-07 14:50:10 +0300

    Added new code fo init.lua file

--- ubuntu/nvim/init.lua
+++ ubuntu/nvim/init.lua
@@ -1,31 +1,17 @@
 require('conf.basic')
 require('conf.keymaps')
-
-local Plug = require 'usermod.vimplug'
-
-Plug.begin('~/.config/nvim/plugged')
-
-Plug 'moll/vim-bbye'
-Plug('junegunn/goyo.vim', {ft = 'markdown'})
-Plug('VonHeikemen/rubber-themes.vim', {
-  run = function()
-    vim.opt.termguicolors = true
-    vim.cmd('colorscheme rubber')
-  end
-})
-Plug('b3nj5m1n/kommentary', {
-  config = function()
-    local cfg = require('kommentary.config')
-
-    cfg.configure_language('default', {
-      prefer_single_line_comments = true,
-    })
-  end
-})
-
-Plug '9mm/vim-closer'
-Plug 'neovim/nvim-lspconfig'
-Plug 'junegunn/fzf.vim'
-
-Plug.ends()
-
+require('conf.plugins')
+require('conf.colorschemes')
+require('conf.completion')
+require('conf.lsp')
+require('conf.treesitter')
+require('conf.autopairs')
+require('conf.comment')
+require('conf.gitsigns')
+require('conf.nvimtree')
+require('conf.bufferline')
+require('conf.lualine')
+require('conf.toggleterm')
+require('conf.impatient')
+require('conf.indentline')
+require('conf.alpha')
